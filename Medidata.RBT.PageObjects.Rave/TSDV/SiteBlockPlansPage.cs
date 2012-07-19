573a267b1e8f6dd28b0d4a70a6b9734eb814f831
Debbie <dsilberberg@mdsol.com>
2012-07-19 15:30:27 -0400

    add tsdv page

---
+++ Medidata.RBT.PageObjects.Rave/TSDV/SiteBlockPlansPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using Medidata.RBT.SeleniumExtension;
+using TechTalk.SpecFlow;
+
+namespace Medidata.RBT.PageObjects.Rave
+{
+    public class SiteBlockPlansPage : RavePageBase
+    {
+
+        public IPage InactivatePlan()
+        {
+            IWebElement elem = Browser.TryFindElementByLinkText("Inactivate");
+            if (elem != null)
+            {
+                elem.Click();
+                TestContext.CurrentPage.As<PageBase>().GetAlertWindow().Accept();
+            }
+            return this;
+        }
+
+        public IPage ActivatePlan()
+        {
+            IWebElement elem = Browser.TryFindElementByLinkText("Activate");
+            if (elem != null)
+            {
+                elem.Click();
+                TestContext.CurrentPage.As<PageBase>().GetAlertWindow().Accept();
+            }
+            return this;
+        }
+
+        public override string URL
+        {
+            get
+            {
+                return "Modules/Reporting/TSDV/SiteBlockPlans.aspx";
+            }
+        }
+    }
+}
