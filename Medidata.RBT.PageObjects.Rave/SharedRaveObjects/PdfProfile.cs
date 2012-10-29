92358cd16fa8db847a53025567c468ee796eae5d
Soyab Kapadia <skapadia@mdsol.com>
2012-10-29 13:02:38 -0400

    Seeding support for pdf profiles
    s
    
    Step def for pdf profile seeding

---
+++ Medidata.RBT.PageObjects.Rave/SharedRaveObjects/PdfProfile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medidata.RBT;
+using TechTalk.SpecFlow;
+using Medidata.RBT.SeleniumExtension;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using Medidata.RBT.PageObjects.Rave.SiteAdministration;
+using Medidata.RBT.SharedRaveObjects;
+using Medidata.RBT.PageObjects.Rave.Configuration;
+using Medidata.RBT.PageObjects.Rave.PDF;
+
+namespace Medidata.RBT.PageObjects.Rave.SharedRaveObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PdfProfile : SeedableObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="profileName"></param>
+        /// <param name="seed"></param>
+        public PdfProfile(string profileName, bool seed = false)
+            :base(profileName)
+        {
+            if (!UID.HasValue)
+            {
+                UID = Guid.NewGuid();
+                Name = profileName;
+
+                if (seed)
+                    Seed();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void NavigateToSeedPage()
+        {
+            LoginPage.LoginUsingDefaultUserFromAnyPage();
+            TestContext.CurrentPage.As<HomePage>().ClickLink("Configuration");
+            TestContext.CurrentPage.As<ConfigurationPage>().ClickLink("PDF Settings");
+        }
+        /// <summary>
+        /// Create a unique name for the pdf profile by appending a TID.
+        /// </summary>
+        public override void MakeUnique()
+        {
+            UniqueName = Name + TID;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void CreateObject()
+        {
+            TestContext.CurrentPage.As<PDFConfigProfilesPage>().CreateNewPdfProfile(UniqueName);
+        }
+    }
+}
---
+++ Medidata.RBT.PageObjects.Rave/TableModels/PdfProfileModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medidata.RBT.PageObjects.Rave
+{
+    /// <summary>
+    /// This model is to configure pdf settings
+    /// </summary>
+    public class PdfProfileModel
+    {
+        /// <summary>
+        /// Name to be used to create pdf
+        /// </summary>
+        public string ProfileName { get; set; }
+        /// <summary>
+        /// Annotation to be selected for pdf
+        /// </summary>
+        public string Annotation { get; set; }
+    }
+}
