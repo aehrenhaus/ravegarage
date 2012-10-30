fd49348e0be0e01b2a473c84db286632d560220f
zcao <zcao@mdsol.com>
2012-10-29 20:34:16 -0400

    Role is now using the new Excel API

--- Medidata.RBT.PageObjects.Rave/SharedRaveObjects/Role.cs
+++ Medidata.RBT.PageObjects.Rave/SharedRaveObjects/Role.cs
@@ -51,40 +51,34 @@ namespace Medidata.RBT.PageObjects.Rave.SharedRaveObjects
 
 			FileLocation = RBTConfiguration.Default.UploadPath + @"\Roles\" + fileName;
 
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(FileLocation);
-            List<XmlNode> worksheets = new List<XmlNode>(xmlDoc.GetElementsByTagName("Worksheet").Cast<XmlNode>());
 
-            //Make the role name unique
-            XmlNode userWorksheet = worksheets.Where(x => x.Attributes["ss:Name"].Value.ToLower() == "edc roles").FirstOrDefault();
-            XmlNode table = (userWorksheet.ChildNodes.Cast<XmlNode>()).Where(x => x.Name.ToLower() == "table").FirstOrDefault();
-            List<XmlNode> rows = (table.ChildNodes.Cast<XmlNode>()).Where(x => x.Name.ToLower() == "row").ToList();
-            List<XmlNode> firstRowCells = new List<XmlNode>(rows[0].ChildNodes.Cast<XmlNode>());
-            List<XmlNode> secondRowCells = new List<XmlNode>(rows[1].ChildNodes.Cast<XmlNode>());
+			using (var excel = new ExcelWorkbook(FileLocation))
+			{
+				var rolesRaw = excel.GetWorksheetValueRange("EDC Roles");
+				var rolesTable = new ExcelTable(rolesRaw);
+				var actionsRaw = excel.GetWorksheetValueRange("Role Actions");
+				var actionsTable = new ExcelTable(actionsRaw);
+				
+				var name = rolesTable[1, "Name"];
+				UniqueName = name + TID;
+				rolesTable[1, "Name"] = UniqueName;
 
-            int roleCell = firstRowCells.FindIndex(x => x.InnerText.ToLower() == "name") + 1; //Add 1, because the index in the XML is 1 indexed, not 0 indexed
-            List<XmlNode> secondRowCellsWithIndexAttr = secondRowCells.Where(x => x.Attributes["ss:Index"] != null).ToList();
-            string oldRole = secondRowCellsWithIndexAttr.FirstOrDefault(x => x.Attributes["ss:Index"].Value == roleCell.ToString()).ChildNodes[0].InnerText;
-            int secondRowRoleIndex = secondRowCells.FindIndex(x => x.ChildNodes[0].InnerText == oldRole);
-            UniqueName = oldRole + TID;
-            secondRowCells[secondRowRoleIndex].ChildNodes[0].InnerText = UniqueName;
+				actionsTable[1, "Role"] = UniqueName;
 
-            //Make the role actions name match the unique name
-            XmlNode roleActionsWorksheet = worksheets.Where(x => x.Attributes["ss:Name"].Value.ToLower() == "role actions").FirstOrDefault();
-            XmlNode roleActionsTable = (roleActionsWorksheet.ChildNodes.Cast<XmlNode>()).Where(x => x.Name.ToLower() == "table").FirstOrDefault();
-            List<XmlNode> roleActionsRows = (roleActionsTable.ChildNodes.Cast<XmlNode>()).Where(x => x.Name.ToLower() == "row").ToList();
-            List<XmlNode> roleActionsFirstRowCells = new List<XmlNode>(roleActionsRows[0].ChildNodes.Cast<XmlNode>());
-            List<XmlNode> roleActionsSecondRowCells = new List<XmlNode>(roleActionsRows[1].ChildNodes.Cast<XmlNode>());
-            //Make the role name in role actions unique
-            int roleActionsSecondRowCellsRoleNameCell = roleActionsFirstRowCells
-                .FindIndex(x => x.InnerText.ToLower() == "role") + 1; //Add 1, because the index in the XML is 1 indexed, not 0 indexed
-            int roleActionSecondRowRoleIndex = roleActionsSecondRowCells.FindIndex(x => x.ChildNodes[0].InnerText == oldRole);
-            roleActionsSecondRowCells[roleActionSecondRowRoleIndex].ChildNodes[0].InnerText = UniqueName;
+				//Create a unique version of the file to upload
+				UniqueFileLocation = MakeFileLocationUnique(FileLocation);
 
-            //Create a unique version of the file to upload
-            UniqueFileLocation = MakeFileLocationUnique(FileLocation);
+				
+				excel.SetWorksheetValueRange("EDC Roles", rolesRaw);
+				excel.SetWorksheetValueRange("Role Actions", actionsRaw);
 
-            xmlDoc.Save(UniqueFileLocation);
+				UniqueFileLocation = MakeFileLocationUnique(FileLocation);
+				excel.SaveAs(UniqueFileLocation);
+
+		
+			}
+
+
         }
 
         /// <summary>
