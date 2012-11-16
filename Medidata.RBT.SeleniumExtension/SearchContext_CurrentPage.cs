2e9fbd68173ec02e05f0eb8fa96e0c62ef38c7a9
Seth Micalizzi <smicalizzi@mdsol.com>
2012-11-16 15:45:39 -0500

    CI Housekeeping, fix intermittent issue on slower computers where pages weren't having enough time to load before the "As" method resolved

---
+++ Medidata.RBT.SeleniumExtension/SearchContext_CurrentPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Collections.ObjectModel;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+using System.Text.RegularExpressions;
+
+namespace Medidata.RBT.SeleniumExtension
+{
+	public static partial class ISearchContextExtend
+	{
+        /// <summary>
+        /// This is a bit of a hack to use web driver wait to wait a controlled amount for the page to load.
+        /// </summary>
+        /// <param name="context">The browser CurrentPage's browser instance</param>
+        /// <param name="urlCheckMethod">A method to check that the URL is the correct page</param>
+        /// <returns></returns>
+        public static IWebElement CheckURLIsCorrect(this ISearchContext context, Func<IWebDriver, IWebElement> urlCheckMethod)
+        {
+            return waitForElement(context, urlCheckMethod);
+        }
+	}
+}
