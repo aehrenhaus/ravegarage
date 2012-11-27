574142161350f2b0bbbd4bb888b41a2df67f89c4
Seth Micalizzi <smicalizzi@mdsol.com>
2012-11-27 15:51:42 -0500

    CI Specflow: US18745_DT11148_AddEvents.feature Fixing text equivalence so that it checks each element, not just the body element text. Fix assorted localization issues

--- Medidata.RBT/PageObjectInterfaces/IVerifySomethingExists.cs
+++ Medidata.RBT/PageObjectInterfaces/IVerifySomethingExists.cs
@@ -4,6 +4,6 @@ namespace Medidata.RBT
 
 	public interface IVerifySomethingExists 
 	{
-		bool VerifySomethingExist(string areaIdentifier, string type, string identifier);
+		bool VerifySomethingExist(string areaIdentifier, string type, string identifier, bool exactMatch = false);
 	}
 }
