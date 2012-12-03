f404b122d9ad4f8e41743189e4363bc5dcc630b0
Soyab Kapadia <skapadia@mdsol.com>
2012-12-03 11:49:12 -0500

    Added exception capture when lab standard group is not specified

--- Medidata.RBT.PageObjects.Rave/SharedRaveObjects/UploadedDraft.cs
+++ Medidata.RBT.PageObjects.Rave/SharedRaveObjects/UploadedDraft.cs
@@ -76,13 +76,17 @@ namespace Medidata.RBT.PageObjects.Rave.SharedRaveObjects
 				draftTable[1, "ProjectName"] = Project.UniqueName;
 
                 //Lab standard group
-                string labStandardGroupName = draftTable[1, "LabStandardGroup"].ToString();
-
-                if (!string.IsNullOrEmpty(labStandardGroupName))
+                try
                 {
-                    StandardGroup labStandardGroup = TestContext.GetExistingFeatureObjectOrMakeNew(labStandardGroupName, () => new StandardGroup(labStandardGroupName));
-                    draftTable[1, "LabStandardGroup"] = labStandardGroup.UniqueName;
+                    string labStandardGroupName = draftTable[1, "LabStandardGroup"].ToString();
+
+                    if (!string.IsNullOrEmpty(labStandardGroupName))
+                    {
+                        StandardGroup labStandardGroup = TestContext.GetExistingFeatureObjectOrMakeNew(labStandardGroupName, () => new StandardGroup(labStandardGroupName));
+                        draftTable[1, "LabStandardGroup"] = labStandardGroup.UniqueName;
+                    }
                 }
+                catch (NullReferenceException) { }
 
 				//draft
 				var oldDraftName = draftTable[1, "DraftName"].ToString();
