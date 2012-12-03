aehrenhaus/ravegarage

commit d3193adf6c8e81aeff526f67442d8469ace7640c

HOUSEKEEPING - replacing FindElements with TryFindElementsBy - should fix problems found in ff for US17446

Medidata.RBT.SeleniumExtension/SearchContext_EnhancedControls.cs
@@ -78,7 +78,7 @@ namespace Medidata.RBT.SeleniumExtension
 		public static ReadOnlyCollection<Textbox> Textboxes(this ISearchContext context, bool allLevel = true)
 		{
 			string xpath = allLevel ? ".//input[@type='text'] | .//textarea" : "./input[@type='text'] | ./textarea";
-			return context.FindElements(By.XPath(xpath)).CastReadOnlyCollection<Textbox>();
+			return context.TryFindElementsBy(By.XPath(xpath)).CastReadOnlyCollection<Textbox>();
 		}
 
 		public static ReadOnlyCollection<Textbox> Textboxes(this ISearchContext context,string xpath)

commit ab70237ca202957442f498c4493c672433cb42e6

HOUSEKEEPING - added more debugging info to output and fixed a colescing issue introduced in previous commit d3193adf6c8e81aeff526f67442d8469ace7640c

Medidata.RBT.PageObjects.Rave/EDC/LandscapeLogField.cs
@@ -105,8 +105,13 @@ namespace Medidata.RBT.PageObjects.Rave
 		public override bool IsElementFocused(ControlType type, int position) 
         {
             var element = GetElementInColumnByRowIDColumnID(type, position);
-            return this.Page.GetFocusElement()
-                .GetAttribute("ID") == element.GetAttribute("ID");
+            var actualId = this.Page.GetFocusElement().GetAttribute("ID");
+            var expectedId = element.GetAttribute("ID");
+            Console.WriteLine(string.Format(
+                "-> LandscapeLogField.IsElementFocused : Actual [{0}], Expected [{1}]",
+                actualId,
+                expectedId));
+            return actualId == expectedId;
         }
 		public override void FocusElement(ControlType type, int position) 
         {

Medidata.RBT.SeleniumExtension/SearchContext_EnhancedControls.cs
@@ -78,7 +78,9 @@ namespace Medidata.RBT.SeleniumExtension
 		public static ReadOnlyCollection<Textbox> Textboxes(this ISearchContext context, bool allLevel = true)
 		{
 			string xpath = allLevel ? ".//input[@type='text'] | .//textarea" : "./input[@type='text'] | ./textarea";
-			return context.TryFindElementsBy(By.XPath(xpath)).CastReadOnlyCollection<Textbox>();
+            var result = context.TryFindElementsBy(By.XPath(xpath))
+                ?? new ReadOnlyCollection<IWebElement>(new List<IWebElement>());
+            return result.CastReadOnlyCollection<Textbox>();
 		}
 
 		public static ReadOnlyCollection<Textbox> Textboxes(this ISearchContext context,string xpath)