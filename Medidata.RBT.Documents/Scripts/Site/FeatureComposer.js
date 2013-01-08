87bbc9af82bef96138fef0de14931e656056c4d7
zcao <zcao@mdsol.com>
2013-01-08 13:12:33 -0500

    modify the FeatureComposer.cshtml UI

--- Medidata.RBT.Documents/Scripts/Site/FeatureComposer.js
+++ Medidata.RBT.Documents/Scripts/Site/FeatureComposer.js
@@ -4,12 +4,6 @@ var clip = null;	//clipboard object of zClipboard
 
 //client side models----------------------
 
-function StepDefCategory(data) {
-	this.StepDefClasses = ko.observableArray(data.StepDefClasses); 
-	this.Name = ko.observable(data.Name);
-} 
-
-
 function Step(step, comment) {
 	this.Step = ko.observable(step);
 	this.Comment = ko.observable(comment);
@@ -37,16 +31,40 @@ function CreateModel() {
 		self.Steps.remove(step);
 	}
 
-	self.LoadStepDefs = function () {
-		$.getJSON(serviceURL, function (allData) {
+
+
+	self.LoadStepDefs = function (callback) {
+		$.getJSON(siteURL + "api/docapi", function (allData) {
 			self.ToolsLoaded(true);
 			//you can map the returned json to a defined client model, 
 			// or set it directly to StepDefCategories
 			//$.map(allData, function (item) { return new StepDefCategory(item) });
 
+			//add a Visible property to category model and the stepDefClasses inside cateogry
+			for (var i = 0; i < allData.length; i++) {
+				allData[i].Visible = ko.observable(true);
+				for (var j = 0; j < allData[i].StepDefClasses.length; j++) {
+					allData[i].StepDefClasses[j].Visible = ko.observable(true);
+					allData[i].StepDefClasses[j].toggleVisible = function (item, event) {
+						item.Visible(!item.Visible());
+					}
+				}
+			}
+
 			self.StepDefCategories(allData);
-			//alert(self.StepDefCategories()[0].StepDefClasses[0].StepDefs[0].Name);
+
 			MakeAccordion();
+
+			//set them to false after accordion is made, because otherwise accordion will not be properly applied to hidden stepDefClass
+			for (var i = 0; i < allData.length; i++) {
+				for (var j = 0; j < allData[i].StepDefClasses.length; j++) {
+					allData[i].StepDefClasses[j].Visible(false);
+				}
+			}
+
+			if (callback)
+				callback();
+
 		});
 
 	}
@@ -54,7 +72,7 @@ function CreateModel() {
 	function MakeAccordion() {
 		//jquery accordion applied to step defs
 		$(".accordionCate").accordion({
-			alwaysOpen: false,
+
 			heightStyle: "content",
 			collapsible: true, active: false,
 			autoHeight: false
@@ -62,8 +80,10 @@ function CreateModel() {
 
 		//jquery accordion applied to step defs
 		$(".accordion").accordion({
+		
 			heightStyle: "content",
-			collapsible: true
+			collapsible: true, active: false,
+			autoHeight: false
 		});
 	}
 }
@@ -71,14 +91,35 @@ function CreateModel() {
 
 //page load
 
+function setupClipboard() {
+	ZeroClipboard.setMoviePath(relURL + "zeroclipboard/ZeroClipboard2.swf");
+
+	clip = new ZeroClipboard.Client();
+
+	clip.addEventListener('mouseOver', function (client) {
+		var text = "";
+		var steps = model.Steps();
+
+		for (var i = 0; i < steps.length; i++) {
+			text += steps[i].Step() + "\r\n";
+		}
+
+
+		clip.setText(text);
+	});
+
+	clip.glue('d_clip_button');
+
+}
+
 $(function () {
 
 	//knockout client side MVVM bindnig
 	model = new CreateModel();
 	ko.applyBindings(model);
-	model.LoadStepDefs();
-	
-	
+	model.LoadStepDefs(setupClipboard); 
+
+
 
 	//	//deal with the input can't be focused bug,
 	//	$('#sortable input').bind('click.sortable mousedown.sortable', function (ev) {
@@ -138,28 +179,6 @@ $(function () {
 	});
 
 	//
-
-
-
-
-	clip = new ZeroClipboard.Client();
-
-	clip.addEventListener('mouseOver', function (client) {
-		var text = "";
-		var steps = model.Steps();
-
-		for (var i = 0; i < steps.length; i++) {
-			text += steps[i].Step() + "\r\n";
-		}
-
-
-		clip.setText(text);
-	});
-
-
-	clip.glue('d_clip_button');
-
-
 
 });
 
