aehrenhaus/ravegarage

commit c0d50daaff617cc0f1c424f4938d47c6ce6df571

MCC46591 - Support for MCC46591.feature - Support for localization tests around PDFs and various other areas.

Medidata.RBT.PageObjects.Rave/PDF/FileRequestCreateRequestPageBase.cs
@@ -26,7 +26,15 @@ namespace Medidata.RBT.PageObjects.Rave
             SelectStudy(args.Study, args.Environment);
             SelectLocale(args.Locale);
             SelectRole(args.Role);
+            PerformPDFSpecificSelections(args);
+            SelectFormExclusions(args.FormExclusions.Split(',').ToList());
         }
+
+        /// <summary>
+        /// Perform specific PDF selections that are only viable for either data PDF or blank PDF
+        /// </summary>
+        /// <param name="args">The PDF arguments you want to select</param>
+        public abstract void PerformPDFSpecificSelections(PDFCreationModel args);
 
         /// <summary>
         /// Enter the desired pdf name in the textbox
@@ -103,6 +111,21 @@ namespace Medidata.RBT.PageObjects.Rave
                     (role, () => new Role(role)).UniqueName;
 
                 ChooseFromDropdown("Role", roleName);
+            }
+        }
+
+        /// <summary>
+        /// Select the form exclusions for the pdf generator
+        /// </summary>
+        /// <param name="formExclusions">List of forms to exclude</param>
+        public void SelectFormExclusions(List<string> formExclusions)
+        {
+            //Open Form Exclusions box if it isn't already open
+            IWebElement formsDiv = Browser.TryShowArea("Forms_div", "Forms_ShowHideBtn");
+            foreach (string formToExclude in formExclusions)
+            {
+                //get checkbox next to form and check it
+                formsDiv.TryFindElementBy(By.XPath(".//td[contains(text(), '" + formToExclude.Trim() + "')]/../td/input")).EnhanceAs<Checkbox>().Check();
             }
         }
 

Medidata.RBT.SeleniumExtension/EnhancedControls/HtmlTable.cs
@@ -56,8 +56,20 @@ namespace Medidata.RBT.SeleniumExtension
 
 				return dataTable.Rows.Any(dr =>
 				{
-					//use trim because there could be spaces 
-					return dr.All(x => x.Value.Trim() ==tdTextSelector( tds[indexMapping[x.Key]]));
+                    List<IWebElement> trsToReturn = new List<IWebElement>();
+                    foreach (KeyValuePair<string, string> x in dr)
+                    {
+                        int value = -1;
+                        indexMapping.TryGetValue(x.Key, out value);
+                        if (value > -1)
+                        {
+                            IWebElement td = tds[value];
+                            //use trim because there could be spaces 
+                            if (x.Value.Trim() == tdTextSelector(td))
+                                return true;
+                        }
+                    }
+                    return false;
 				});
 
 			});

Medidata.RBT.Features.Rave/Features/PDF/MCC46591.feature
@@ -1,6 +1,5 @@
 ﻿@MCC-46591
 @ignore
-
 Feature: MCC-46591 Log Lines in Blank/Blank Annotated PDFs are not displayed.
 
 #Note: Known DT12954 - Annotated PDFs cannot display all dictionary entries for Dictionaries with a large Number of Entries

Medidata.RBT.Features.Rave/Medidata.RBT.Features.Rave.csproj
@@ -101,11 +101,6 @@
       <DesignTime>True</DesignTime>
       <DependentUpon>US18531_AM2_DT13626.feature</DependentUpon>
     </Compile>
-    <Compile Include="Features\Architect\MCC-42707.feature.cs">
-      <AutoGen>True</AutoGen>
-      <DesignTime>True</DesignTime>
-      <DependentUpon>MCC-42707.feature</DependentUpon>
-    </Compile>
     <Compile Include="Features\Architect\US17446.feature.cs">
       <DependentUpon>US17446.feature</DependentUpon>
       <AutoGen>True</AutoGen>
@@ -482,10 +477,6 @@
     <None Include="Features\AM2\US18531_AM2_DT13626.feature">
       <Generator>SpecFlowSingleFileGenerator</Generator>
       <LastGenOutput>US18531_AM2_DT13626.feature.cs</LastGenOutput>
-    </None>
-    <None Include="Features\Architect\MCC-42707.feature">
-      <Generator>SpecFlowSingleFileGenerator</Generator>
-      <LastGenOutput>MCC-42707.feature.cs</LastGenOutput>
     </None>
     <None Include="Features\Architect\US17446.feature">
       <Generator>SpecFlowSingleFileGenerator</Generator>

Medidata.RBT.Features.Rave/Steps/LoginSteps.cs
@@ -2,6 +2,7 @@
 using Medidata.RBT.PageObjects.Rave;
 using Medidata.RBT.PageObjects.Rave.SharedRaveObjects;
 using System;
+using Medidata.RBT.SeleniumExtension;
 
 
 namespace Medidata.RBT.Features.Rave
@@ -58,7 +59,7 @@ namespace Medidata.RBT.Features.Rave
         [StepDefinition(@"I log out of Rave")]
         public void ILogOutOfRave()
         {
-            CurrentPage.ClickLink("Logout");
+            CurrentPage.Browser.TryFindElementByPartialID("LogoutLink").Click();
         }
 	}
 }

Medidata.RBT.Features.Rave/Steps/RaveSteps.cs
@@ -5,6 +5,7 @@ using TechTalk.SpecFlow;
 using Medidata.RBT.PageObjects.Rave;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
 
 namespace Medidata.RBT.Features.Rave
 {
@@ -40,8 +41,9 @@ namespace Medidata.RBT.Features.Rave
         {
 			string str = WebTestContext.Storage[RaveSteps.TripReports] as string;
 			Assert.IsFalse(String.IsNullOrEmpty(str));
+            string noWhitespacePDFtext = Regex.Replace(str, @"\s", "");
 
-			if (CreateSymbolsTable(table).Any(s => !str.Contains(s)))
+            if (CreateSymbolsTable(table).Any(s => !noWhitespacePDFtext.Contains(Regex.Replace(s, @"\s", ""))))
                 Assert.Fail();
         }
 

Medidata.RBT.PageObjects.Rave/PDF/FileRequestCreateBlankRequestPage.cs
@@ -22,17 +22,22 @@ namespace Medidata.RBT.PageObjects.Rave
         public FileRequestPage CreateBlankPDF(PDFCreationModel args)
         {
             base.CreatePDF(args);
-
-            SelectCRFVersion(args.CRFVersion);
-
-            ClickLink("Save");
-
+            Browser.TryFindElementByPartialID("SaveLnkBtn").Click();
 
             Browser.TryFindElementById("_ctl0_Content_SearchCriteriaLabel", true, 10);
             //To make sure saved pdf is avaialble in the list before we try to generate it
             Browser.Navigate().Refresh();
 
             return new FileRequestPage();
+        }
+
+        /// <summary>
+        /// Perform specific PDF selections that are only viable for blank PDF
+        /// </summary>
+        /// <param name="args">The PDF arguments you want to select</param>
+        public override void PerformPDFSpecificSelections(PDFCreationModel args)
+        {
+            SelectCRFVersion(args.CRFVersion);
         }
 
         /// <summary>

Medidata.RBT.PageObjects.Rave/PDF/FileRequestCreateDataRequestPage.cs
@@ -28,10 +28,6 @@ namespace Medidata.RBT.PageObjects.Rave
             if (!string.IsNullOrEmpty(args.Locale) && args.Locale == "LLocalization Test")
                 isLocalizationTest = true;
 
-            SelectSiteGroup(args.SiteGroup);
-            SelectSite(args.Site);
-            SelectSubject(args.Subject);
-
             string saveLinkText = "Save";
 
             if (isLocalizationTest)
@@ -44,6 +40,17 @@ namespace Medidata.RBT.PageObjects.Rave
             this.Browser.Navigate().Refresh();
 
             return new FileRequestPage();
+        }
+
+        /// <summary>
+        /// Perform specific PDF selections that are only viable for data PDF
+        /// </summary>
+        /// <param name="args">The PDF arguments you want to select</param>
+        public override void PerformPDFSpecificSelections(PDFCreationModel args)
+        {
+            SelectSiteGroup(args.SiteGroup);
+            SelectSite(args.Site);
+            SelectSubject(args.Subject);
         }
 
         /// <summary>

commit efd8d3b8284528b5fb43b20511ca804e0039a964

CI Housekeeping, fix issues caused by commit c0d50daaff617cc0f1c424f4938d47c6ce6df571 Fix null reference on FileRequestCreateRequestPageBase.cs Fix improper functionality in FindMatchRows after refactor (It was finding matches matching at least one case, rather than all of the cases)

Medidata.RBT.PageObjects.Rave/PDF/FileRequestCreateRequestPageBase.cs
@@ -27,7 +27,8 @@ namespace Medidata.RBT.PageObjects.Rave
             SelectLocale(args.Locale);
             SelectRole(args.Role);
             PerformPDFSpecificSelections(args);
-            SelectFormExclusions(args.FormExclusions.Split(',').ToList());
+            if(args.FormExclusions != null)
+                SelectFormExclusions(args.FormExclusions.Split(',').ToList());
         }
 
         /// <summary>

Medidata.RBT.SeleniumExtension/EnhancedControls/HtmlTable.cs
@@ -54,23 +54,22 @@ namespace Medidata.RBT.SeleniumExtension
 
 				//Is there ***ANY*** datarow that ***ALL*** columns match the html row's columns
 
-				return dataTable.Rows.Any(dr =>
-				{
-                    List<IWebElement> trsToReturn = new List<IWebElement>();
+                return dataTable.Rows.Any(dr =>
+                {
                     foreach (KeyValuePair<string, string> x in dr)
                     {
-                        int value = -1;
-                        indexMapping.TryGetValue(x.Key, out value);
-                        if (value > -1)
+                        int positionOfTd = -1;
+                        indexMapping.TryGetValue(x.Key, out positionOfTd);
+                        if (positionOfTd > -1)
                         {
-                            IWebElement td = tds[value];
+                            IWebElement td = tds[positionOfTd];
                             //use trim because there could be spaces 
-                            if (x.Value.Trim() == tdTextSelector(td))
-                                return true;
+                            if (!x.Value.Trim().Equals(tdTextSelector(td)))
+                                return false;
                         }
                     }
-                    return false;
-				});
+                    return true;
+                });
 
 			});
 