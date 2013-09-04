946127f9a4025c33489bb793ce912af3dedacdd1
Pramod Somashekar <psomashekar@mdsol.com>
2013-09-04 22:06:00 +0900

    Update comments

--- Medidata.RBT.PageObjects.Rave/SeedableObjects/ScriptUtility.cs
+++ Medidata.RBT.PageObjects.Rave/SeedableObjects/ScriptUtility.cs
@@ -10,7 +10,7 @@ namespace Medidata.RBT.PageObjects.Rave.SeedableObjects
                /// <summary>
         /// Create a script utility entry if it is not already in the dictionary of FeatureObject
         /// </summary>
-        /// <param name="scriptName">Name of the script utility XML file (ie. gotten from MyMedidata or custom packaged </param>
+        /// <param name="scriptName">Name of the script utility XML file (ie. gotten from MyMedidata or custom packaged) </param>
 		public ScriptUtility(string scriptName)
         {
 	        UniqueName = scriptName;
