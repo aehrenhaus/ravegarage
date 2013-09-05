959f0f28ee4ed50c2a6d10efd442ff28a4531e74
Soyab Kapadia <skapadia@mdsol.com>
2013-09-05 17:54:46 -0400

    Added a fix for spreadsheets that are empty but formatted resulting in out of memory exception that cannot be handled.

--- Medidata.RBT.Common.Steps/ExcelSteps.cs
+++ Medidata.RBT.Common.Steps/ExcelSteps.cs
@@ -117,7 +117,11 @@ namespace Medidata.RBT.Common.Steps
 
 			using (var excel = new ExcelWorkbook(fileName))
 			{
-				var sheet = excel.OpenTableForEdit(name);
+                ExcelTable sheet;
+                if (table.Rows.Count == 1 && table.Rows[0].All(row => row.Value == "")) //A check for tables will single row of all empty data for verification against empty worksheet
+                    sheet = excel.OpenTableForEdit(name, table.Header.Count.ToString()); //table column count is passed as range to get only 2 rows from worksheet using table column count
+                else
+                    sheet = excel.OpenTableForEdit(name);
 
 				foreach (var column in table.Header)
 				{
